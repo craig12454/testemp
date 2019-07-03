cf7be3950d90fa2093ff2eacdbf72b4e1d341b22
Craig Ives <ivesie23@gmail.com>
2019-07-03 11:33:38 +0100

    toggle nav

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function openNav() {
+    var x = document.getElementById("hide__nav");
+    if(x.style.display == 'block')
+        x.style.display = 'none';
+    else
+        x.style.display = 'block';
+}
